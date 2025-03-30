37304ec1830020b9d23a1bd6bde613b67af1f2db
nikhilnirwan <nikhilnirwan216@gmail.com>
2025-03-30 15:07:37 +0530

    Update mogodb url

--- dbconnect.py
+++ dbconnect.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://username:password@cluster1.kpemw.mongodb.net/fastapicrud"
+uri = "mongodb url add here"
 client = MongoClient(uri, server_api = ServerApi('1'))
 
 db = client.todo_db
